a0636346971daeaff9de2c18299a74d3ef389b83
Andr-off <and_ron@mail.ru>
2020-05-12 17:54:35 +0500

    add routes

--- server/index.js
+++ server/index.js
@@ -4,10 +4,12 @@ import serve from 'koa-static';
 import path from 'path';
 import _ from 'lodash';
 import koaWebpack from 'koa-webpack';
+import Router from 'koa-router';
 import Rollbar from 'rollbar';
 import dotenv from 'dotenv';
 
 import webpackConfig from '../webpack.config';
+import addRoutes from '../routes';
 
 export default () => {
   dotenv.config();
@@ -17,29 +19,45 @@ export default () => {
   const rollbar = new Rollbar({
     accessToken: process.env.ACCESS_TOKEN,
     captureUncaught: true,
-    captureUnhandledRejections: true
+    captureUnhandledRejections: true,
   });
 
   app.use(serve(path.join(__dirname, '../public')));
 
-  koaWebpack({
-    config: webpackConfig,
-  })
-    .then((midleware) => app.use(midleware));
+  if (process.env.NODE_ENV === 'development') {
+    koaWebpack({
+      config: webpackConfig,
+    }).then((midleware) => app.use(midleware));
+  }
+
+  const router = new Router();
+
+  addRoutes(router);
+
+  app.use(router.routes());
+  app.use(router.allowedMethods());
 
   const pug = new Pug({
     viewPath: path.join(__dirname, '../views'),
+    noCache: process.env.NODE_ENV === 'development',
+    debug: process.env.NODE_ENV === 'development',
+    compileDebug: true,
     locals: {},
     basedir: path.join(__dirname, '../views'),
     helperPath: [
       { _ },
+      { urlFor: (...args) => router.url(...args) },
     ],
   });
 
   pug.use(app);
 
-  app.use(async (ctx) => {
-    await ctx.render('index');
+  app.use(async (ctx, next) => {
+    try {
+      await next();
+    } catch (err) {
+      rollbar.error(err, ctx.request);
+    }
   });
 
   return app;
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
-  entry: './src/index.js',
+  entry: ['./src/index.js'],
   output: {
     path: path.join(__dirname, 'public', 'assets'),
     filename: 'main.js',
