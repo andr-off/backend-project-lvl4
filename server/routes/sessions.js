5f29585541dcda5d9d30ef8c9483a1e9a501eadb
Andr-off <and_ron@mail.ru>
2020-06-03 22:11:47 +0500

    add sessions and sessions route

---
+++ server/routes/sessions.js
@@ -0,0 +1,32 @@
+import buildFormObj from '../lib/formObjectBuilder';
+import encrypt from '../lib/secure';
+import db from '../models';
+
+const { User } = db;
+
+export default (router) => {
+  router
+    .get('newSession', '/session/new', async (ctx) => {
+      const data = {};
+      await ctx.render('sessions/new', { f: buildFormObj(data) });
+    })
+    .post('session', '/session', async (ctx) => {
+      const { email, password } = ctx.request.body.form;
+      const user = await User.findOne({
+        where: {
+          email,
+        },
+      });
+      if (user && user.passwordDigest === encrypt(password)) {
+        ctx.session.userId = user.id;
+        ctx.redirect(router.url('root'));
+        return;
+      }
+      ctx.flash.set('email or password were wrong');
+      await ctx.render('sessions/new', { f: buildFormObj({ email }) });
+    })
+    .delete('session', '/session', (ctx) => {
+      ctx.session = {};
+      ctx.redirect(router.url('root'));
+    });
+};
