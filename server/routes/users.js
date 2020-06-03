c3cbc2ecf27417caeafc10992c0b8376c279e3cd
Andr-off <and_ron@mail.ru>
2020-06-03 17:08:07 +0500

    add user routes

---
+++ server/routes/users.js
@@ -0,0 +1,28 @@
+import db from '../models';
+import buildFormObj from '../lib/formObjectBuilder';
+
+const { User } = db;
+
+export default (router) => {
+  router
+    .get('users', '/users', async (ctx) => {
+      const users = await User.findAll();
+      await ctx.render('users', { users });
+    })
+    .get('newUser', '/users/new', async (ctx) => {
+      const user = User.build();
+      await ctx.render('users/new', { f: buildFormObj(user) });
+    })
+    .post('users', '/users', async (ctx) => {
+      const { request: { body: { form } } } = ctx;
+      const user = User.build(form);
+      console.log(ctx.request.body);
+      try {
+        await user.save();
+        ctx.flash.set('User has been created');
+        ctx.redirect(router.url('root'));
+      } catch (e) {
+        await ctx.render('users/new', { f: buildFormObj(user, e) });
+      }
+    });
+};
