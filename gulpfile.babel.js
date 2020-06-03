1239b92bd97d2ce5ef68c5fb49eab6fa725313c6
Andr-off <and_ron@mail.ru>
2020-06-03 16:30:41 +0500

    add console task in gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,19 @@
 import gulp from 'gulp';
+import repl from 'repl';
+
+import container from './container';
 import getServer from './server';
 
 gulp.task('server', (cb) => {
   getServer().listen(process.env.PORT || 4000, cb);
+});
+
+gulp.task('console', () => {
+  const replServer = repl.start({
+    prompt: 'App console >',
+  });
+
+  Object.keys(container).forEach((key) => {
+    replServer.context[key] = container[key];
+  });
 });
