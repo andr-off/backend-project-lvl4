05898ab9ee1bf36f9a5199f66ced87044c10cecf
Andr-off <and_ron@mail.ru>
2020-06-03 17:01:30 +0500

    add user model

---
+++ server/models/index.js
@@ -0,0 +1,33 @@
+import path from 'path';
+import fs from 'fs';
+import Sequelize from 'sequelize';
+
+import configByEnv from '../config/config.json';
+
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = configByEnv[env];
+const db = {};
+
+const sequelize = new Sequelize(config);
+
+fs.readdirSync(__dirname)
+  .filter((filepath) => {
+    const { ext } = path.parse(filepath);
+    return ext && (filepath !== basename) && (ext === '.js');
+  })
+  .forEach((filepath) => {
+    const model = sequelize.import(path.join(__dirname, filepath));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+export default db;
