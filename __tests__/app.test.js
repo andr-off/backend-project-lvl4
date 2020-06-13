f19c208c8e9ec37020e5982b701d9662c2247311
Andr-off <and_ron@mail.ru>
2020-06-13 12:47:25 +0500

    improve tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,61 +8,54 @@ import getApp from '../server';
 
 describe('requests', () => {
   const { User } = db;
-  User.sync();
-
   let server;
-  let query;
+  let data;
   let formData;
-  let wrongData;
-  let resAfterAddingUser;
+  let url;
 
-  beforeAll(() => {
+
+  beforeAll(async () => {
     expect.extend(matchers);
+
+    await User.sync();
+
+    data = {
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
+
+    formData = { form: data };
+
+    const { id } = await User.create(data);
+    url = `/users/${id}`;
   });
 
   beforeEach(async () => {
     server = getApp().listen();
-    query = request.agent(server);
-    formData = {
-      form: {
-        firstName: faker.name.firstName(),
-        lastName: faker.name.lastName(),
-        email: faker.internet.email(),
-        password: faker.internet.password(),
-      },
-    };
-    wrongData = {
-      form: {
-        email: faker.internet.email(),
-        password: faker.internet.password(),
-      },
-    };
-    resAfterAddingUser = await query
-      .post('/users')
-      .type('form')
-      .send(formData);
   });
 
   test('GET 200', async () => {
-    const res = await query
+    const res = await request.agent(server)
       .get('/');
     expect(res).toHaveHTTPStatus(200);
   });
 
   test('GET 404', async () => {
-    const res = await query
+    const res = await request.agent(server)
       .get('/wrong-path');
     expect(res).toHaveHTTPStatus(404);
   });
 
   test('GET /session/new', async () => {
-    const res = await query
+    const res = await request.agent(server)
       .get('/session/new');
     expect(res).toHaveHTTPStatus(200);
   });
 
   test('POST /session', async () => {
-    const res = await query
+    const res = await request.agent(server)
       .post('/session')
       .type('form')
       .send(formData);
@@ -70,61 +63,70 @@ describe('requests', () => {
   });
 
   test('POST /session (errors)', async () => {
-    const res = await query
+    const res = await request.agent(server)
       .post('/session')
       .type('form')
-      .send(wrongData);
+      .send({ form: { email: '', password: '' } });
     expect(res).toHaveHTTPStatus(422);
   });
 
   test('DELETE /session', async () => {
-    const res1 = await query
+    const res1 = await request.agent(server)
       .post('/session')
       .type('form')
       .send(formData);
     expect(res1).toHaveHTTPStatus(302);
 
-    const res2 = await query
+    const res2 = await request.agent(server)
       .delete('/session');
     expect(res2).toHaveHTTPStatus(302);
   });
 
   test('GET /users/new', async () => {
-    const res = await query
+    const res = await request.agent(server)
       .get('/users/new');
     expect(res).toHaveHTTPStatus(200);
   });
 
   test('POST /users', async () => {
-    expect(resAfterAddingUser).toHaveHTTPStatus(302);
+    const res = await request.agent(server)
+      .post('/users')
+      .type('form')
+      .send({
+        form: {
+          firstName: faker.name.firstName(),
+          lastName: faker.name.lastName(),
+          email: faker.internet.email(),
+          password: faker.internet.password(),
+        },
+      });
+    expect(res).toHaveHTTPStatus(302);
   });
 
   test('POST /users (errors)', async () => {
-    const res1 = await query
+    const res1 = await request.agent(server)
       .post('/users')
       .type('form')
       .send({ form: { email: '', password: formData.form.password } });
     expect(res1).toHaveHTTPStatus(422);
 
-    const res2 = await query
+    const res2 = await request.agent(server)
       .post('/users')
       .type('form')
       .send({ form: { email: formData.form.email, password: '' } });
     expect(res2).toHaveHTTPStatus(422);
   });
 
-  test('GET /users/:id/settings', async () => {
-    const url = resAfterAddingUser.headers.location;
+  test('GET /users/:id/profile', async () => {
+    const urlToProfile = `${url}/profile`;
 
-    const res2 = await query
-      .get(url);
-    expect(res2).toHaveHTTPStatus(200);
+    const res = await request.agent(server)
+      .get(urlToProfile);
+    expect(res).toHaveHTTPStatus(200);
   });
 
   test('PATCH /users/:id', async () => {
-    const url = resAfterAddingUser.headers.location.match(/\/users\/\d+/)[0];
-
-    const res = await query
+    const res = await request.agent(server)
       .patch(url)
       .type('form')
       .send({
@@ -137,9 +139,7 @@ describe('requests', () => {
   });
 
   test('PATCH /users/:id (errors)', async () => {
-    const url = resAfterAddingUser.headers.location.match(/\/users\/\d+/)[0];
-
-    const res = await query
+    const res = await request.agent(server)
       .patch(url)
       .type('form')
       .send({
@@ -150,9 +150,7 @@ describe('requests', () => {
   });
 
   test('DELETE /users/:id', async () => {
-    const url = resAfterAddingUser.headers.location.match(/\/users\/\d+/)[0];
-
-    const res = await query
+    const res = await request.agent(server)
       .delete(url);
     expect(res).toHaveHTTPStatus(302);
   });
