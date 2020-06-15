c4cffe9b808d7d732f268254354a796c410b9398
Andr-off <and_ron@mail.ru>
2020-06-15 12:23:14 +0500

    add route to user page

--- server/routes/users.js
+++ server/routes/users.js
@@ -11,13 +11,38 @@ export default (router, container) => {
       const users = await User.findAll();
       await ctx.render('users', { users });
     })
+
     .get('newUser', '/users/new', async (ctx) => {
       const user = User.build();
       await ctx.render('users/new', { f: buildFormObj(user) });
     })
+
+    .get('userPage', '/users/:id', async (ctx) => {
+      const { id } = ctx.params;
+
+      try {
+        const user = await User.findOne({
+          where: {
+            id,
+          },
+        });
+
+        if (!user) {
+          ctx.status = 404;
+          return;
+        }
+
+        await ctx.render('users/page', { user });
+      } catch (e) {
+        log(e);
+        ctx.status = 500;
+      }
+    })
+
     .post('users', '/users', async (ctx) => {
       const { request: { body: { form } } } = ctx;
       const user = User.build(form);
+
       try {
         await user.save();
         ctx.flash.set('User has been created');
