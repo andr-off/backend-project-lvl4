a967896f88a710e549e6b93669b10d15ab3c6e69
Andr-off <and_ron@mail.ru>
2020-06-19 16:23:23 +0500

    improve tests

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/fc7e01428d254740644a/maintainability)](https://codeclimate.com/github/andr-off/backend-project-lvl4/maintainability)
 [![Test Coverage](https://api.codeclimate.com/v1/badges/fc7e01428d254740644a/test_coverage)](https://codeclimate.com/github/andr-off/backend-project-lvl4/test_coverage)
 
-[go to website](https://cool-task-manager.herokuapp.com/)
+[go to the website](https://cool-task-manager.herokuapp.com/)
--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,189 +7,153 @@ import getApp from '../server';
 
 
 describe('requests', () => {
-  const { User } = db;
+  let req;
   let server;
-  let formData;
-  let url;
-  let newData;
 
+  const { User } = db;
+
+  const rootUrl = '/';
+  const wrongUrl = '/wrong-path';
+  const userUrl1 = '/users/1';
+  const wrongUserUrl = '/users/5';
+  const newUserUrl = '/users/new';
+  const usersUrl = '/users';
+  const editUserUrl1 = '/users/1/edit';
+  const sessionUrl = '/session';
+  const newSessionUrl = '/session/new';
+
+  const getDataForUser = () => ({
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    email: faker.internet.email().toLowerCase(),
+    password: faker.internet.password(),
+  });
+
+  const dataForUser1 = getDataForUser();
+  const dataForUser2 = getDataForUser();
+
+  const formData = {
+    form: dataForUser1,
+  };
+
+  const formData2 = {
+    form: dataForUser2,
+  };
+
+  const getQueriesTestCases = [
+    ['GET 200', rootUrl, 200],
+    ['GET 404', wrongUrl, 404],
+    ['GET /session/new', newSessionUrl, 200],
+    ['GET /users/new', newUserUrl, 200],
+    ['GET /users/:id', userUrl1, 200],
+    ['GET /users/:id 404', wrongUserUrl, 404],
+  ];
+
+  const postQueriesTestCases = [
+    ['POST /session', sessionUrl, formData, 302],
+    ['POST /users', usersUrl, { form: getDataForUser() }, 302],
+    ['POST /session (errors)', sessionUrl, { form: { email: '', password: '' } }, 422],
+    ['POST /users (errors)', usersUrl, { form: { ...dataForUser1, email: '' } }, 422],
+  ];
+
+  const patchQueriesTestCases = [
+    ['PATCH /users/:id', userUrl1, { form: getDataForUser() }, 302],
+    ['PATCH /users/:id (errors1)', userUrl1, { form: { ...formData.form, email: '' } }, 422],
+    ['PATCH /users/:id (errors2)', userUrl1, { form: { ...formData.form, password: '' } }, 422],
+  ];
 
   beforeAll(async () => {
     expect.extend(matchers);
-
-    await User.sync();
-
-    const data = {
-      firstName: faker.name.firstName(),
-      lastName: faker.name.lastName(),
-      email: faker.internet.email().toLowerCase(),
-      password: faker.internet.password(),
-    };
-
-    formData = { form: data };
-
-    const { id } = await User.create(data);
-    url = `/users/${id}`;
   });
 
   beforeEach(async () => {
+    await User.drop();
+    await User.sync();
+
     server = getApp().listen();
-    newData = {
-      form: {
-        firstName: faker.name.firstName(),
-        lastName: faker.name.lastName(),
-        email: faker.internet.email(),
-        password: faker.internet.password(),
-      },
-    };
+    req = request.agent(server);
+
+    await User.create(dataForUser1);
+    await User.create(dataForUser2);
   });
 
-  test('GET 200', async () => {
-    const res = await request.agent(server)
-      .get('/');
-    expect(res).toHaveHTTPStatus(200);
+  test.each(getQueriesTestCases)('%s', async (_testName, url, httpStatus) => {
+    const res = await req
+      .get(url);
+    expect(res).toHaveHTTPStatus(httpStatus);
   });
 
-  test('GET 404', async () => {
-    const res = await request.agent(server)
-      .get('/wrong-path');
-    expect(res).toHaveHTTPStatus(404);
-  });
-
-  test('GET /session/new', async () => {
-    const res = await request.agent(server)
-      .get('/session/new');
-    expect(res).toHaveHTTPStatus(200);
-  });
-
-  test('POST /session', async () => {
-    const res = await request.agent(server)
-      .post('/session')
+  test.each(postQueriesTestCases)('%s', async (_testName, url, data, httpStatus) => {
+    const res = await req
+      .post(url)
       .type('form')
-      .send(formData);
-    expect(res).toHaveHTTPStatus(302);
+      .send(data);
+    expect(res).toHaveHTTPStatus(httpStatus);
   });
 
-  test('POST /session (errors)', async () => {
-    const res = await request.agent(server)
-      .post('/session')
+  test.each(patchQueriesTestCases)('%s', async (_testName, url, data, httpStatus) => {
+    const res = await req
+      .patch(url)
       .type('form')
-      .send({ form: { email: '', password: '' } });
-    expect(res).toHaveHTTPStatus(422);
+      .send(data);
+    expect(res).toHaveHTTPStatus(httpStatus);
   });
 
   test('DELETE /session', async () => {
-    const res1 = await request.agent(server)
+    const res1 = await req
       .post('/session')
       .type('form')
       .send(formData);
     expect(res1).toHaveHTTPStatus(302);
 
-    const res2 = await request.agent(server)
+    const res2 = await req
       .delete('/session');
     expect(res2).toHaveHTTPStatus(302);
   });
 
-  test('GET /users/new', async () => {
-    const res = await request.agent(server)
-      .get('/users/new');
-    expect(res).toHaveHTTPStatus(200);
+  test('DELETE /users/:id', async () => {
+    const res1 = await req
+      .delete(userUrl1);
+    expect(res1).toHaveHTTPStatus(302);
+
+    const res2 = await req
+      .get(userUrl1);
+    expect(res2).toHaveHTTPStatus(404);
   });
 
-  test('POST /users', async () => {
-    const res = await request.agent(server)
-      .post('/users')
-      .type('form')
-      .send(newData);
-    expect(res).toHaveHTTPStatus(302);
-  });
-
-  test('POST /users (errors)', async () => {
-    const res = await request.agent(server)
-      .post('/users')
-      .type('form')
-      .send({
-        form:
-        {
-          firstName: '',
-          lastName: '',
-          email: '',
-          password: formData.form.password,
-        },
-      });
-    expect(res).toHaveHTTPStatus(422);
-  });
 
   test('GET /users/:id/edit', async () => {
-    const urlToEditPage = `${url}/edit`;
-
-    const res1 = await request.agent(server)
-      .get(urlToEditPage);
+    const res1 = await req
+      .get(editUserUrl1);
     expect(res1).toHaveHTTPStatus(403);
 
-    const res2 = await request.agent(server)
+    const res2 = await req
       .post('/session')
       .type('form')
       .send(formData);
 
     const cookie = res2.headers['set-cookie'];
 
-    const res3 = await request.agent(server)
+    const res3 = await req
       .set('Cookie', cookie)
-      .get(urlToEditPage);
+      .get(editUserUrl1);
     expect(res3).toHaveHTTPStatus(200);
-  });
 
-  test('GET /users/:id', async () => {
-    const res = await request.agent(server)
-      .get(url);
-    expect(res).toHaveHTTPStatus(200);
-  });
+    const res4 = await req
+      .delete('/session');
+    expect(res4).toHaveHTTPStatus(302);
 
-  test('PATCH /users/:id', async () => {
-    const res = await request.agent(server)
-      .patch(url)
+    const res5 = await req
+      .post('/session')
       .type('form')
-      .send(newData);
-    expect(res).toHaveHTTPStatus(302);
-  });
+      .send(formData2);
 
-  test('PATCH /users/:id (errors)', async () => {
-    const wrongData1 = {
-      form: {
-        ...formData.form,
-        email: '',
-      },
-    };
+    const cookie2 = res5.headers['set-cookie'];
 
-    const wrongData2 = {
-      form: {
-        ...formData.form,
-        password: '',
-      },
-    };
-
-    const res1 = await request.agent(server)
-      .patch(url)
-      .type('form')
-      .send(wrongData1);
-    expect(res1).toHaveHTTPStatus(422);
-
-    const res2 = await request.agent(server)
-      .patch(url)
-      .type('form')
-      .send(wrongData2);
-    expect(res2).toHaveHTTPStatus(422);
-  });
-
-  test('DELETE /users/:id', async () => {
-    const res1 = await request.agent(server)
-      .delete(url);
-    expect(res1).toHaveHTTPStatus(302);
-
-    const res2 = await request.agent(server)
-      .get(url);
-    expect(res2).toHaveHTTPStatus(404);
+    const res6 = await req
+      .set('Cookie', cookie2)
+      .get(editUserUrl1);
+    expect(res6).toHaveHTTPStatus(403);
   });
 
   afterEach((done) => {
