0d9905974fbe00b54e92d8aebcf3c411f98d0827
Andr-off <and_ron@mail.ru>
2020-06-25 16:13:07 +0500

    add tests for taskstatuses

---
+++ __tests__/taskstatuses.test.js
@@ -0,0 +1,35 @@
+import request from 'supertest';
+import matchers from 'jest-supertest-matchers';
+import db from '../server/models';
+
+import getApp from '../server';
+
+describe('requests to /taskstatuses', () => {
+  let req;
+  let server;
+
+  const { TaskStatus } = db;
+
+  beforeAll(async () => {
+    expect.extend(matchers);
+  });
+
+  beforeEach(async () => {
+    await TaskStatus.drop();
+    await TaskStatus.sync();
+
+    server = getApp().listen();
+    req = request.agent(server);
+  });
+
+  test('GET /taskstatuses', async () => {
+    const res = await req
+      .get('/taskstatuses');
+    expect(res).toHaveHTTPStatus(200);
+  });
+
+  afterEach((done) => {
+    server.close();
+    done();
+  });
+});
