69ec1c6d4213aca6022f79fedb96d0a3b669f19f
Andr-off <and_ron@mail.ru>
2020-09-17 15:15:22 +0500

    refactor user update request

--- server/routes/users.js
+++ server/routes/users.js
@@ -46,7 +46,6 @@ export default (router, container) => {
 
       try {
         await user.save();
-
         ctx.flash('info', i18next.t('flash.users.create.success'));
         ctx.redirect(router.url('newSession'));
       } catch (e) {
@@ -67,61 +66,47 @@ export default (router, container) => {
         throw new container.errors.NotFoundError();
       }
 
+      const userData = form || password;
+
       if (form) {
-        form.email = normalizeEmail(form.email);
-        form.firstName = normalizeName(form.firstName);
-        form.lastName = normalizeName(form.lastName);
-
-        try {
-          await user.update(form);
-
-          ctx.flash('info', i18next.t('flash.users.patch.success'));
-          ctx.redirect(router.url('editUser', id));
-        } catch (e) {
+        userData.email = normalizeEmail(userData.email);
+        userData.firstName = normalizeName(userData.firstName);
+        userData.lastName = normalizeName(userData.lastName);
+      } else {
+        if (user.passwordDigest !== encrypt(password.current)) {
           ctx.status = 422;
           ctx.flash('error', i18next.t('flash.users.patch.error'));
           await ctx.render('users/edit', {
-            f: buildFormObj(user, e),
-            p: buildFormObj(user, e, 'password'),
+            f: buildFormObj(user),
+            p: buildFormObj(user, {
+              errors: [
+                { path: 'current', message: i18next.t('validation.users.wrongPassword') },
+              ],
+            }, 'password'),
           });
+
+          return;
         }
 
-        return;
-      }
+        if (password.password !== password.confirm) {
+          ctx.status = 422;
+          ctx.flash('error', i18next.t('flash.users.patch.error'));
+          await ctx.render('users/edit', {
+            f: buildFormObj(user),
+            p: buildFormObj(user, {
+              errors: [
+                { path: 'password', message: i18next.t('validation.users.passwordsMustBeEqual') },
+                { path: 'confirm', message: i18next.t('validation.users.passwordsMustBeEqual') },
+              ],
+            }, 'password'),
+          });
 
-      if (user.passwordDigest !== encrypt(password.current)) {
-        ctx.status = 422;
-        ctx.flash('error', i18next.t('flash.users.patch.error'));
-        await ctx.render('users/edit', {
-          f: buildFormObj(user),
-          p: buildFormObj(user, {
-            errors: [
-              { path: 'current', message: i18next.t('validation.users.wrongPassword') },
-            ],
-          }, 'password'),
-        });
-
-        return;
-      }
-
-      if (password.password !== password.confirm) {
-        ctx.status = 422;
-        ctx.flash('error', i18next.t('flash.users.patch.error'));
-        await ctx.render('users/edit', {
-          f: buildFormObj(user),
-          p: buildFormObj(user, {
-            errors: [
-              { path: 'password', message: i18next.t('validation.users.passwordsMustBeEqual') },
-              { path: 'confirm', message: i18next.t('validation.users.passwordsMustBeEqual') },
-            ],
-          }, 'password'),
-        });
-
-        return;
+          return;
+        }
       }
 
       try {
-        await user.update(password);
+        await user.update(userData);
 
         ctx.flash('info', i18next.t('flash.users.patch.success'));
         ctx.redirect(router.url('editUser', id));
